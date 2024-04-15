15dd45d8cff666a6b0384f4b18a52e474fd6301c
caiolimaa.2001@gmail.com <caiolimaa.2001@gmail.com>
2024-04-15 12:29:36 -0300

    step09 finished

--- 05_FirstStepsInJavaWithSpringBoot/api-java/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 05_FirstStepsInJavaWithSpringBoot/api-java/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.exceptions.ResourceNotFoundException;
-import br.com.erudio.model.Person;
 import br.com.erudio.services.PersonServices;
 
 @RestController
@@ -29,25 +29,25 @@ public class PersonController {
 	private PersonServices service;
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
+	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
 	
 	@GetMapping(value = "/{id}",
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") Long id) {	
+	public PersonVO findById(@PathVariable(value = "id") Long id) {	
 		return service.findById(id);
 	}
 	
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create(@RequestBody Person person) {	
+	public PersonVO create(@RequestBody PersonVO person) {	
 		return service.create(person);
 	}
 	
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody Person person) {	
+	public PersonVO update(@RequestBody PersonVO person) {	
 		return service.update(person);
 	}
 	

513721fe9c188807338496fe74d937213b11af95
caiolimaa.2001@gmail.com <caiolimaa.2001@gmail.com>
2024-04-22 11:30:52 -0300

    step12 finished

--- 05_FirstStepsInJavaWithSpringBoot/api-java/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
+++ 05_FirstStepsInJavaWithSpringBoot/api-java/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
@@ -3,6 +3,9 @@ package br.com.erudio.data.vo.v1;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,12 +13,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+@JsonPropertyOrder({"id", "first_name", "last_name", "address", "gender"})
 public class PersonVO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@JsonProperty("first_name")
 	private String firstName;
+	
+	@JsonProperty("last_name")
 	private String lastName;
 	private String address;
 	private String gender;

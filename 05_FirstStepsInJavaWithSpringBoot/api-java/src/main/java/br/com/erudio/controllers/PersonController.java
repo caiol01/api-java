dd3134512846ab9371b23f8a748f7cd37d03a89d
caiolimaa.2001@gmail.com <caiolimaa.2001@gmail.com>
2024-04-22 11:15:45 -0300

    step11 finished

--- 05_FirstStepsInJavaWithSpringBoot/api-java/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 05_FirstStepsInJavaWithSpringBoot/api-java/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -22,7 +22,7 @@ import br.com.erudio.exceptions.ResourceNotFoundException;
 import br.com.erudio.services.PersonServices;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 	
 	@Autowired
